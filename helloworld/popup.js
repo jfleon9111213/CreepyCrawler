b974a46245c0f585cde2cf2f08852d53de2a3b0c
jfleon9111213 <30878868+jfleon9111213@users.noreply.github.com>
2019-01-28 01:36:24 -0800

    Add files via upload

---
+++ helloworld/popup.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    chrome.tabs.getSelected(null, function (tab) {
+        var link = document.createElement('a');
+        link.href = tab.url;
+        $('#host').html("Host : " + link.href);
+    })
+});
