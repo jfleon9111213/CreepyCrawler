a28cba2205122f6fcdd01323c3096ec274f9ceca
jfleon9111213 <30878868+jfleon9111213@users.noreply.github.com>
2019-01-28 14:13:06 -0800

    Added comments

--- helloworld/popup.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    chrome.tabs.getSelected(null, function (tab) {
-        var link = document.createElement('a');
-        link.href = tab.url;
-        $('#host').html("Host : " + link.href);
-    })
-});
