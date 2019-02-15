8f56e7a40fd0e5fdf58dbb4f70dfb630571004c2
jfleon9111213 <30878868+jfleon9111213@users.noreply.github.com>
2019-02-15 13:22:03 -0800

    Added call to Azure API. JSON formatting, boilerpipe, and scope/lifetime issues to be fixed

--- urlretrieve.js
+++ urlretrieve.js
@@ -1,6 +1,8 @@
 //Retrieves the URL of the current tab, gets the html file
 //of the boilerpipe extraction, retrieves the highlighted text, and
-//ouputs it.
+//sends it to the Key Phrases Azure API. Formatting POST request needed.
+//Scoping and boilerpipe problem surfaced.
+
 chrome.tabs.getSelected(null, function (tab) {
     var link = document.createElement('a');
     link.href = tab.url;
@@ -9,19 +11,35 @@ chrome.tabs.getSelected(null, function (tab) {
     } else {
         var request = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    request.open("GET", " http://boilerpipe-web.appspot.com/extract?url=" + link.href, true);
+    var words = "";
+    request.open("GET", "http://boilerpipe-web.appspot.com/extract?url=" + link.href, true);
     request.send();
+
     request.onreadystatechange = function() {
         if(request.readyState == 4) {
             document.open();
 	    document.write(request.responseText);
 	    var list = document.getElementsByClassName("x-boilerpipe-mark1");
+            document.close();
             var i;
-            var words = "";
             for(i = 0; i < list.length; i++) {
                 words = words.concat(list[i].innerText + " ");
             }
-            alert(words);
-        };
-    };
+            //alert (words);
+        }
+    }
+    
+
+    var documents = {language: "en", id: "1", text: words};
+    //var myJSON = JSON.parse(documents);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases", true);
+    xhr.setRequestHeader("Content-Type","application/json");
+    xhr.setRequestHeader("Ocp-Apim-Subscription-Key", "51567aff9ae14b64b15f7540a32a54d9");
+    xhr.send(documents);
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4) {
+            alert("Success:  " + xhr.responseText);
+        }
+    }   
 })
