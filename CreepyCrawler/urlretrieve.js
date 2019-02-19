fcd9eed06d53cc575e82ea94e2e0501c71cdc00f
Angela Gadon <apgadon@gmail.com>
2019-02-19 13:55:03 -0800

    Modified urlretrieve.js to include Key Phrase API

--- CreepyCrawler/urlretrieve.js
+++ CreepyCrawler/urlretrieve.js
@@ -6,4 +6,28 @@ $(document).ready(function() {
         var request = $.ajax({ url: link.href, success: function(data) { alert(data); } });
         $('#host').html("HTML : " + request);
     })
+    
+    //sending HTML file to Key Phrase API call
+    var documents =
+    {
+        "documents":[
+            {
+                language: "en",
+                id: "1",
+                text: request
+            }
+        ]
+    };
+    var myJSON = JSON.stringify(documents);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases", true);
+    xhr.setRequestHeader("Content-Type","application/json");
+    xhr.setRequestHeader("Ocp-Apim-Subscription-Key", "51567aff9ae14b64b15f7540a32a54d9");
+    xhr.send(myJSON);
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4) {
+            alert("Success:  " + xhr.responseText);
+        }
+    };
+
 });
