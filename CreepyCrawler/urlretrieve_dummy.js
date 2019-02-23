a171bd2c4c95e090486f9b5c6e375879de3329d9
Angela Gadon <apgadon@gmail.com>
2019-02-22 21:59:20 -0800

    Modified files for UI testing only

---
+++ CreepyCrawler/urlretrieve_dummy.js
@@ -0,0 +1,11 @@
+/*
+ * Holds some URLs for UI testing.
+ * Meant to emulate results of actual urlretrieve.js
+*/
+
+var urlList = [
+	"https://www.vox.com/2019/2/20/18233556/tucker-carlson-rutger-bregman-nowthis-dutch-historian",
+	"https://www.nytimes.com/2019/02/22/podcasts/the-daily/isis-american-women.html",
+	"https://www.usnews.com/news/sports/articles/2019-02-22/saric-criticizes-world-cup-qualifying-system-as-croatia-out"
+	];
+alert(urlList);
