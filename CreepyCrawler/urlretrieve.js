48c94662ee8b4c98707f1109710992cfe8f87b56
Angela Gadon <apgadon@gmail.com>
2019-03-19 20:59:25 -0700

    Modified slider in Filters

--- CreepyCrawler/urlretrieve.js
+++ CreepyCrawler/urlretrieve.js
@@ -170,5 +170,5 @@ output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
+  slider.innerHTML = this.value;
 }
